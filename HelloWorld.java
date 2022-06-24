8a7351be77e6f176805c8aeb4dc9fadc82b16cb2
SonakshiPurbey <86663099+SonakshiPurbey@users.noreply.github.com>
2022-06-24 23:15:27 +0530

    Add files via upload

---
+++ HelloWorld.java
@@ -0,0 +1,11 @@
+// Java Hello World Program
+public class HelloWorld {
+
+    public static void main(String[] args) {
+
+        // Display the message on command prompt
+        System.out.println("Chankok.com - Hello World!"); 
+
+    }	
+
+}
